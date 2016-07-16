30fdb3dd945b76e34555179664774cd53bef5507
timeyyy <timeyyy_da_man@hotmail.com>
2016-07-16 16:57:46 +0200

    improve return types

--- src/nvim/mark_extended.h
+++ src/nvim/mark_extended.h
@@ -4,17 +4,20 @@
 /* #include "nvim/ex_cmds_defs.h" // for exarg_T */
 #include "nvim/mark_extended_defs.h"
 #include "nvim/lib/kbtree.h"
+#include "nvim/lib/kvec.h"
 
 #define EXTMARK_MAXLEN 24
 #define extmark_pos_cmp(a, b) (pos_cmp((a).fmark.mark, (b).fmark.mark))
 
 typedef struct ExtendedMark ExtendedMark;
 struct ExtendedMark {
+  char *name;
   fmark_T fmark;
   ExtendedMark *next;
   ExtendedMark *prev;
 };
 
+typedef kvec_t(cstr_t) ExtmarkNames;
 typedef Map(cstr_t, ptr_t) ExtendedMarkPtr;
 KBTREE_INIT(extmarks, ExtendedMark, extmark_pos_cmp)
 
