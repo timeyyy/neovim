258dd0cc4abeb1a1689f0ea9287780c3cc44cc55
timeyyy <timeyyy_da_man@hotmail.com>
2016-08-03 15:55:51 +0200

    trying to cleanup tests

--- test/functional/api/mark_extended_spec.lua
+++ test/functional/api/mark_extended_spec.lua
@@ -2,6 +2,7 @@
 local helpers = require('test.functional.helpers')(after_each)
 local Screen = require('test.functional.ui.screen')
 
+local request = helpers.request
 local eq = helpers.eq
 local neq = helpers.neq
 local buffer = helpers.buffer
@@ -9,27 +10,37 @@ local insert = helpers.insert
 local feed = helpers.feed
 local wait = helpers.wait
 
-describe('creating a new mark', function()
+describe('Extmarks buffer api', function()
   local screen
-  helpers.clear()
-  helpers.nvim('set_option', 'shell', helpers.nvim_dir .. '/shell-test')
-  helpers.nvim('set_option', 'shellcmdflag', 'EXE')
-  screen = Screen.new(20, 4)
-  screen:attach(false)
+  local curbuf
 
-  it('tims', function()
-    local buf = helpers.nvim('get_current_buffer')
-    local mark_name = 0
-    local ns_string = "my-fancy-plugin"
-    local ns_string2 = "my-fancy-plugin2"
-    local row = 0
-    local col = 2
+  marks = {2, 3, 4}
+  positions = {{0, 1,}, {0, 3}, {0, 4}}
 
-    local init_text = "12345"
+  ns_string = "my-fancy-plugin"
+  ns_string2 = "my-fancy-plugin2"
+  init_text = "12345"
+  row = 0
+  col = 2
+
+  -- before_each(function()
+    helpers.clear()
+    helpers.nvim('set_option', 'shell', helpers.nvim_dir .. '/shell-test')
+    helpers.nvim('set_option', 'shellcmdflag', 'EXE')
+    screen = Screen.new(20, 4)
+    screen:attach(false)
+
     insert(init_text)
+  -- end)
 
-    -- setup the namespace
-    local rv = buffer('mark_ns_init', ns_string)
+  buf = request('vim_get_current_buffer')
+
+  -- after_each(function()
+    -- screen:detach()
+  -- end)
+
+  it('add and query namespaces', function()
+    rv = buffer('mark_ns_init', ns_string)
     eq(1, rv)
     -- rv = buffer('mark_ns_init', ns_string)
     -- eq(0, rv)
@@ -39,42 +50,46 @@ describe('creating a new mark', function()
     rv = buffer('mark_ns_ids')
     eq({ns_string, 1}, rv[1])
     eq({ns_string2, 2}, rv[2])
+  -- end)
 
-
-    local ns = 1
-    -- mark the "2"
-    rv = buffer('mark_set', buf, ns, mark_name, row, col)
+  -- it('adds, updates  and deletes marks', function()
+    -- ns = buffer('mark_ns_init', ns_string)
+    -- rv = buffer('mark_set', buf, ns, marks[1], positions[1][1], positions[1][2])
+    rv = request('buffer_mark_index', buf, ns, marks[1])
+    rv = buffer('mark_index', buf, ns, marks[1])
+    local rv = buffer('mark_set', {buf, ns, 0, 0, 2})
     wait()
     eq(1, rv)
-    rv = buffer('mark_index', buf, ns, mark_name)
-    eq({mark_name, row, col}, rv)
+    rv = buffer('mark_index', buf, ns, marks[1])
+    eq({marks[1], positions[1][2], positions[1][2]}, rv)
     -- Test adding a second mark works
-    local mark_name2 = 1
-    rv = buffer('mark_set', buf, ns, mark_name2, 0, 3)
+    rv = buffer('mark_set', buf, ns, marks[2], positions[2][1], positions[2][2])
     wait()
     eq(1, rv)
-
-    -- Test an update
+    -- Test an update, (same pos)
+    rv = buffer('mark_set', buf, ns, marks[1], positions[1][1], positions[1][2])
+    wait()
+    eq(2, rv)
+    rv = buffer('mark_index', buf, ns, marks[2])
+    eq({marks[2], positions[2][1], positions[2][2]}, rv)
+    -- Test an update, (new pos)
+    row = positions[1][1]
+    col = positions[1][2] + 1
     rv = buffer('mark_set', buf, ns, mark_name, row, col)
     wait()
     eq(2, rv)
-    row = 0
-    col = col + 1
-    rv = buffer('mark_set', buf, ns, mark_name, row, col)
-    wait()
-    eq(2, rv)
-    rv = buffer('mark_index', buf, ns, mark_name)
-    eq({mark_name, row, col}, rv)
+    rv = buffer('mark_index', buf, ns, marks[2])
+    eq({marks[2], row, col}, rv)
 
     -- remove the test marks
-    rv = buffer('mark_unset', buf, ns, mark_name)
+    rv = buffer('mark_unset', buf, ns, marks[1])
     eq(1, rv)
-    rv = buffer('mark_unset', buf, ns, mark_name2)
+    rv = buffer('mark_unset', buf, ns, marks[2])
     eq(1, rv)
+  -- end)
 
+  -- it('gets information on current marks ', function()
     -- add some more marks
-    marks = {2, 3, 4}
-    positions = {{0, 1,}, {0, 3}, {0, 4}}
     for i, m in ipairs(marks) do
         rv = buffer('mark_set', buf, ns, m, positions[i][1], positions[i][2])
         eq(1, rv)
@@ -83,7 +98,9 @@ describe('creating a new mark', function()
     for i, m in ipairs(marks) do
         eq({m, positions[i][1], positions[i][2]}, rv[i])
     end
+  -- end)
 
+  -- it('returns ranges correctly', function()
     -- Using next
     rv = buffer('mark_next', buf, ns, marks[1])
     eq({marks[2], positions[2][1], positions[2][2]}, rv)
@@ -110,8 +127,9 @@ describe('creating a new mark', function()
     -- eq({marks[3], positions[3][1], positions[3][2]}, rv[1])
     -- eq({marks[2], positions[2][1], positions[2][2]}, rv[2])
     -- eq({marks[1], positions[1][1], positions[1][2]}, rv[3])
+    -- end)
 
-    -- index, next, prev, nextrange, prevrange, work with positional indexes
+  -- it('returns ranges correctly when using position indexes', function()
     rv = buffer('mark_index', buf, ns, positions[1])
     eq({marks[1], positions[1][1], positions[1][2]}, rv)
 
@@ -132,7 +150,9 @@ describe('creating a new mark', function()
     -- eq({marks[3], positions[3][1], positions[3][2]}, rv[1])
     -- eq({marks[2], positions[2][1], positions[2][2]}, rv[2])
     -- eq({marks[1], positions[1][1], positions[1][2]}, rv[3])
+    -- end)
 
+  -- it('marks move with text inserts', function()
     -- marks move with text inserts?
     -- added_text = "999"
     -- feed('0') -- move cursor to start of line
