d95803f2951ef3168bfcf9355987c997b86b91e5
timeyyy <timeyyy_da_man@hotmail.com>
2017-03-19 19:37:07 +0100

    extmarks: lint

--- src/nvim/misc1.c
+++ src/nvim/misc1.c
@@ -1900,17 +1900,9 @@ void appended_lines(linenr_T lnum, long count)
  */
 void appended_lines_mark(linenr_T lnum, long count)
 {
-<<<<<<< HEAD
-  // Skip mark_adjust when adding a line after the last one, there can't
-  // be marks there. But it's still needed in diff mode.
-  if (lnum + count < curbuf->b_ml.ml_line_count || curwin->w_p_diff) {
-    mark_adjust(lnum + 1, (linenr_T)MAXLNUM, count, 0L, false);
-  }
-=======
   mark_adjust(lnum + 1, (linenr_T)MAXLNUM, count, 0L, false);
   extmark_adjust(curbuf, lnum + 1, (linenr_T)MAXLNUM, count, 0L,
                  extmarkNoReverse, false);
->>>>>>> extmarks: first commit
   changed_lines(lnum + 1, 0, lnum + 1, count);
 }
 
