4794908d2c0c421d638ef16890db5031be05bb43
Timothy C Eichler <timeyyy_da_man@hotmail.com>
2017-12-14 23:45:20 +0100

    extmarks: add todo for documentation

--- test/functional/api/mark_extended_spec.lua
+++ test/functional/api/mark_extended_spec.lua
@@ -452,7 +452,7 @@ describe('Extmarks buffer api', function()
   end)
 
   -- TODO mark_adjust is busted i vim/neovim
-  pending('marks move with line splits (using enter) #extmarks', function()
+  pending('marks move with line splits (using enter) #fail3', function()
     -- open_line in misc1.c
     -- testing marks below are also moved
     feed("yyP")
